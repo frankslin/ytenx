d3f4d55ad41dcc1c32004b94bd4173f0d14bc1e0
Frank Lin <github@linshuang.info>
2026-06-18 21:47:07 -0700

    Restore IPA chart Flash demos

--- src/cloudflare-worker.js
+++ src/cloudflare-worker.js
@@ -1,5 +1,12 @@
 import { Container } from "@cloudflare/containers";
 
+const REDIRECTS = new Map([
+  [
+    "/byohlyuk/KienxPyan",
+    "https://opencc.byvoid.com/characters-easy-to-misuse/",
+  ],
+]);
+
 export class YtenxContainer extends Container {
   defaultPort = 8000;
   sleepAfter = "30m";
@@ -12,6 +19,12 @@ export class YtenxContainer extends Container {
 
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    const redirect = REDIRECTS.get(url.pathname);
+    if (redirect) {
+      return Response.redirect(redirect, 301);
+    }
+
     const container = env.YTENX_CONTAINER.getByName("ytenx");
     return container.fetch(request);
   },
